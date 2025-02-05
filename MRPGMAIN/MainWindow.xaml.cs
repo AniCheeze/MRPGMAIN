dd0a00b086c7aa60c2c1a2599614fd6933082f49
AniCheeze <daniil.avd06@mail.ru>
2025-02-05 18:35:36 +0700

    First Sync

--- MRPGMAIN/MainWindow.xaml.cs
+++ MRPGMAIN/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MRPGMAIN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
-namespace MRPGMAIN
+
+namespace WpfApp1
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -23,6 +26,61 @@ namespace MRPGMAIN
         public MainWindow()
         {
             InitializeComponent();
+            LinearGradientBrush gradientBrush = new LinearGradientBrush(Color.FromRgb(81, 225, 255), Color.FromRgb(11, 60, 196), new Point(0.5, 0), new Point(0.5, 1));
+            Background = gradientBrush;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (MMORPGBDEntities3 db = new MMORPGBDEntities3())
+            {
+                var log = db.Player.FirstOrDefault(p => p.Login == UserName.Text);
+                var pas = db.Player.FirstOrDefault(p => p.Password == Password.Text);
+                if (log != null && pas != null)
+                {
+                    MessageBox.Show("Вход на сервер ", "Успех", MessageBoxButton.OK, MessageBoxImage.None);
+                    Window1 window1 = new Window1();
+                    window1.NameGG = UserName.Text;
+                    Close();
+                    window1.Show();
+
+                }
+                else
+                {
+                    MessageBox.Show("Такого логина или пароля нет ", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (MMORPGBDEntities3 db = new MMORPGBDEntities3())
+            {
+                var log = db.Player.FirstOrDefault(p => p.Login == UserName.Text);
+                var pas = db.Player.FirstOrDefault(p => p.Password == Password.Text);
+                if (log == null && pas == null)
+                {
+                    Player player = new Player();
+                    SaveData saveData = new SaveData();
+                    saveData.Name = UserName.Text;
+                    saveData.HP = 100;
+                    saveData.ATK = 10;
+                    saveData.DEF = 5;
+                    db.SaveData.Add(saveData);
+                    db.SaveChanges();
+                    MessageBox.Show(saveData.Id.ToString());
+                    player.Login = UserName.Text;
+                    player.Password = Password.Text;
+                    player.IdSaveData = saveData.Id;
+                    db.Player.Add(player);
+                    db.SaveChanges();
+                    MessageBox.Show("Вы успешно зарегистрировались", "Гойда!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Такой логин уже есть", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
---
+++ MainClasses/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainClasses
+{
+    public class Enemy
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int ATK { get; set; }
+        public int DEF { get; set; }
+        public int SPD { get; set; }
+        Enemy(string name, int hP, int aTK, int dEF, int sPD)
+        {
+            Name = name;
+            HP = hP;
+            ATK = aTK;
+            DEF = dEF;
+            SPD = sPD;
+        }
+    }
+    public class Players
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int ATK { get; set; }
+        public int DEF { get; set; }
+        public int SPD { get; set; }
+        Dictionary<string, double> keyValuePairs { get; set; }
+        Players(string name, int hP, int aTK, int dEF, int sPD)
+        {
+            Name = name;
+            HP = hP;
+            ATK = aTK;
+            DEF = dEF;
+            SPD = sPD;
+        }
+    }
+    public interface Items
+    {
+
+        string Name { get; set; }
+        int Status { get; set; }
+    }
+}
+
+
